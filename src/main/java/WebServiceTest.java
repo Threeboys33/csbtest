6e8ff63bf61a87589d3f16db1fc13fd870abf210
Threeboys33 <28470938+Threeboys33@users.noreply.github.com>
2019-12-27 15:00:50 +0800

    Update WebServiceTest.java

--- src/main/java/WebServiceTest.java
+++ src/main/java/WebServiceTest.java
@@ -16,17 +16,17 @@ public class WebServiceTest {
 
     public static void soap() throws RemoteException {
         //设置服务调用的安全信息
-        String apiName = "Third-api-getPiMasterInfo";  //要调用的服务名称
+        String apiName = "xxxx";  //要调用的服务名称
         String apiVersion = "1.0.0";  //要调用的服务版本
-        String ak = "6a9e2ef7f05648b39157204da7b5a301"; //订购服务的accessKey
-        String sk = "asi1HHSYTPfbVZK33qpM1fXLclw="; //订购服务的secrectKey
+        String ak = "xxxx"; //订购服务的accessKey
+        String sk = "xxxx"; //订购服务的secrectKey
         WSParams params = WSParams.create().accessKey(ak).secretKey(sk).api(apiName).version(apiVersion).nonce(true);
         Service service = new Service();// 首先，构造封装Call对象
         Call call = AxisCallWrapper.createCallWrapper(service, params);
         //或者使用WSParams进行参数设置
         // 然后，使用封装Call对象进行方法调用
-        call.setTargetEndpointAddress("http://123.138.75.146:9082/Third-api-getPiMasterInfo/1.0.0/ws2ws");
-        call.setOperationName(new QName("http://cxf.webservice.nhis.zebone.com/", "getPiMasterInfo"));
+        call.setTargetEndpointAddress("http://ip:9082/Third-api-getPiMasterInfo/1.0.0/ws2ws");
+        call.setOperationName(new QName("http://ip/", "getPiMasterInfo"));
         call.setReturnType(org.apache.axis.encoding.XMLType.XSD_STRING);
         call.addParameter("param", // 设置要传递的参数
                 org.apache.axis.encoding.XMLType.XSD_STRING, javax.xml.rpc.ParameterMode.IN);
